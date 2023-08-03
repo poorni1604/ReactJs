d86c0c29952e697f3edb081cd697d64c54167f32
Poornima <poorni160403@gmail.com>
2023-08-03 11:31:05 +0530

    tdy1

---
+++ JS/sample.js
@@ -0,0 +1,16 @@
+var a = 120;
+console.log("This is the value declared for var  before altering type :",a);
+{
+    var a = 7
+    console.log("This is after altering the type :",a)
+}
+console.log("the value after altering: ",a)
+
+let b = 128;
+{
+    let b =7
+    console.log("the value inside the block: ",b)
+}
+console.log("This is the value declared in the let type: ",b);
+const r = 20;
+console.log("This is the value declared in the const type: ",r)
