82752cdb23e1ced48df518826f360d15de3dcd5c
Poornima <poorni160403@gmail.com>
2023-08-03 12:19:54 +0530

    tdy3

--- JS/sample.js
+++ JS/sample.js
@@ -15,10 +15,41 @@ console.log("This is the value declared in the let type: ",b);
 const r = 20;
 console.log("This is the value declared in the const type: ",r)
 
-a = 10
-if (a > 0){
-    console.log("The number is positive")
+a = 0
+if(a === 0){
+    console.log("the number is zero and we are adding one to it",a)
+    a = a + 1
 }
-else{
-    console.log("The number is negative")
+if(a > 0){
+    console.log("the number is positive",a)
 }
+/* else{
+    console.log("the number is negative")
+} */
+//while loop
+i = 0
+while(i< a){
+    console.log(i)
+    i++
+}
+//for loop
+a=10
+for(i=0;i<a;i++){
+    console.log(i)
+} 
+
+ i = 0
+ a = 10 
+ while(i< a){
+    console.log(i)
+    i++
+ }
+ //for loop
+ a=10
+for(i=0;i<a;i++){
+    console.log(i)
+}
+//array
+array=[10,20]
+array.forEach(element => console.log(element))
+
